ca3867f541e4d13dcc2a36c9fd5d5e593e54273f
Kiranpanjam <panjam.kiran@gmail.com>
2020-07-24 18:35:23 -0700

    initial commit

---
+++ assets/js/todolist.js
@@ -0,0 +1,27 @@
+$("ul").on("click", "li", function () {
+  $(this).toggleClass("completed");
+});
+
+$("ul").on("click", "span", function (event) {
+  $(this)
+    .parent()
+    .fadeOut(500, function () {
+      $(this).remove();
+    });
+  event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function (event) {
+  if (event.which === 13) {
+    var todoInput = $(this).val();
+    $("ul").append(
+      "<li><span><i class='far fa-trash-alt'></i></span> " + todoInput + "</li>"
+    );
+    //$("ul").append("<li><span>X</span> " + todoText + "</li>");
+    $(this).val("");
+  }
+});
+
+$(".fa-plus").click(function () {
+  $("input[type='text']").fadeToggle();
+});
